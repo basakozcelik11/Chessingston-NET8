84beee2d92a6950eb56a4aec09388afa1d5e96ea
basakozcelik11 <basak_cortuk@hotmail.com>
2025-06-25 16:24:05 +0100

    Added Queen move logic

--- Chessington.GameEngine/Pieces/Queen.cs
+++ Chessington.GameEngine/Pieces/Queen.cs
@@ -10,7 +10,47 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            var position = board.FindPiece(this);
+            List<Square> squares = new List<Square>();
+            int j = 1;
+
+            for (var i = 0; i < 8; i++)
+                squares.Add(Square.At(position.Row, i));
+
+            for (var i = 0; i < 8; i++)
+                squares.Add(Square.At(i, position.Col));
+
+            squares.RemoveAll(s => s == position);
+
+            for (int i = position.Col + 1; i < 8; i++)
+            {
+                if (position.Row + j < 8)
+                {
+                    squares.Add(Square.At(position.Row + j, i));
+                }
+                if (position.Row - j >= 0)
+                {
+                    squares.Add(Square.At(position.Row - j, i));
+                }
+                j++;
+            }
+
+            //backward direction
+            j = 1;
+            for (int i = position.Col - 1; i >= 0; i--)
+            {
+                if (position.Row + j < 8)
+                {
+                    squares.Add(Square.At(position.Row + j, i));
+                }
+                if (position.Row - j >= 0)
+                {
+                    squares.Add(Square.At(position.Row - j, i));
+                }
+                j++;
+            }
+
+            return squares;
         }
     }
 }
