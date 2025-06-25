d2efb014d8d466d0aa662ed9c55b0d5840d66ee9
basakozcelik11 <basak_cortuk@hotmail.com>
2025-06-25 14:43:25 +0100

    Added rook move
    We have implemented rook to move horizontally and vertically.

--- Chessington.GameEngine/Pieces/Pawn.cs
+++ Chessington.GameEngine/Pieces/Pawn.cs
@@ -11,24 +11,24 @@ namespace Chessington.GameEngine.Pieces
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
             var position = board.FindPiece(this);
-            IEnumerable<Square> squares = new List<Square>();
+            List<Square> squares = new List<Square>();
 
             if (this.Player == Player.Black)
             {
                 if (position.Row == 1)
                 {
-                    squares = squares.Append(new Square(position.Row + 2, position.Col));
+                    squares.Add(Square.At(position.Row + 2, position.Col));
                 }
-                squares = squares.Append(new Square(position.Row + 1, position.Col));
+                squares.Add(Square.At(position.Row + 1, position.Col));
                 
             }
             else
             {
                 if (position.Row == 7)
                 {
-                    squares = squares.Append(new Square(position.Row - 2, position.Col));
+                    squares.Add(Square.At(position.Row - 2, position.Col));
                 }
-                squares = squares.Append(new Square(position.Row - 1, position.Col));
+                squares.Add(Square.At(position.Row - 1, position.Col));
             }
             return squares;
         }
