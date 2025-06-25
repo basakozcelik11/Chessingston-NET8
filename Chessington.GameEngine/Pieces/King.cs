90546b366363dd27ec9a4f3bf3f440b5859b14f5
basakozcelik11 <basak_cortuk@hotmail.com>
2025-06-25 16:59:47 +0100

    added king logic to moves

--- Chessington.GameEngine/Pieces/King.cs
+++ Chessington.GameEngine/Pieces/King.cs
@@ -10,7 +10,21 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            var position = board.FindPiece(this);
+            List<Square> squares = new List<Square>();
+            for (int i = position.Col - 1; i <= position.Col + 1; i++)
+            {
+                for (int j = -1; j < 2; j++)
+                {
+                    var nextSquare = Square.At(position.Row + j, i);
+                    if (board.CheckWithinBounds(nextSquare))
+                    {
+                        squares.Add(nextSquare);
+                    }
+                }              
+            }
+            squares.Remove(position);
+            return squares;
         }
     }
 }
