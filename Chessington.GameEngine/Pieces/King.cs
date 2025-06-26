ab612730e11b0847ea9cbd965dcccba2bc176ac1
basakozcelik11 <basak_cortuk@hotmail.com>
2025-06-26 10:39:44 +0100

    Refactored King to use general move function

--- Chessington.GameEngine/Pieces/King.cs
+++ Chessington.GameEngine/Pieces/King.cs
@@ -11,19 +11,8 @@ namespace Chessington.GameEngine.Pieces
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
             var position = board.FindPiece(this);
-            List<Square> squares = new List<Square>();
-            for (int i = position.Col - 1; i <= position.Col + 1; i++)
-            {
-                for (int j = -1; j < 2; j++)
-                {
-                    var nextSquare = Square.At(position.Row + j, i);
-                    if (board.CheckWithinBounds(nextSquare))
-                    {
-                        squares.Add(nextSquare);
-                    }
-                }              
-            }
-            squares.Remove(position);
+             IEnumerable<Square> squares = AvailableMoves(DiagonalDirection, position, board, 1);
+            squares = squares.Concat(AvailableMoves(LateralDirection, position, board, 1));
             return squares;
         }
     }
